6eb18bb0da060f0b96f4ebb44eba0fbbbd02b0a5
Vicky Tanzil <vicky.tanzil@gmail.com>
2018-09-12 21:25:09 +0200

    Add current_player method

--- lib/move.rb
+++ lib/move.rb
@@ -48,3 +48,15 @@ def play(board)
     current_turn += 1
   end
 end
+
+def turn_count(board)
+  counter = 0
+  board.each do |item|
+    counter += (" " == item) ? 0 : 1
+  end
+  counter
+end
+
+def current_player(board)
+  turn_count(board).even? ? "X" : "O"
+end
